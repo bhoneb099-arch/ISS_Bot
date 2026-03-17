a006a5a7a0aa1ca74ee252a47993ac6d6795d38b
bhoneb099-arch <bhoneb099@gmail.com>
2026-03-18 02:56:23 +0630

    Update main.py

--- main.py
+++ main.py
@@ -234,16 +234,14 @@ Ask questions like:
 # =====================================
 # MAIN BOT
 # =====================================
-async def main():
+def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
-
     app.add_handler(CommandHandler("start", start_command))
     app.add_handler(CommandHandler("iss", iss_command))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     print("Bot running...")
-    await app.run_polling()
+    app.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
+    main()
